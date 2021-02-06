3d750bea1ebc2d5ac69b55f251cbaa3745cc4f24
99003586 <78539604+99003586@users.noreply.github.com>
2021-02-06 17:44:00 +0530

    Update exponential.c
    Commit of exponential function

--- 3_implementation/src/exponential.c
+++ 3_implementation/src/exponential.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include "exponential.h"
+#include <math.h>
+#include <stdlib.h>
 
+void do_exponential()
+{
+    double a,num, p;
+    printf("\nEnter two numbers to find the power \n");
+    printf("number: ");
+    scanf("%lf",&a);
+ 
+    printf("power : ");
+    scanf("%lf",&num);
+ 
+    p=pow(a,num);
+ 
+    printf("\n%lf to the exponential %lf = %lf \n",a,num,p);
+}
