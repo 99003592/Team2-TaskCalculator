02a3df94fb592cb5961f065f7a501a1ead993565
99003588 <78543911+99003588@users.noreply.github.com>
2021-03-01 23:17:01 +0530

    updated

--- 3_implementation/src/addition.c
+++ 3_implementation/src/addition.c
@@ -3,15 +3,16 @@
 
 void do_addition()
 {
-    int n, total=0, k=0, number;
+    int n;
+    float total=0, k=0, number;
     printf("\nEnter the number of elements you want to add:");
     scanf("%d",&n);
     printf("Please enter %d numbers one by one: \n",n);
     while(k<n)
     { 
-        scanf("%d",&number);
+        scanf("%f",&number);
         total=total+number;
         k=k+1;
     }
-    printf("Sum of %d numbers = %d \n",n,total);
+    printf("Sum of %d numbers = %f \n",n,total);
 }
