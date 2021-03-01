1d461c98d1ad024a782f1157642db261d74df540
99003591 <78539816+99003591@users.noreply.github.com>
2021-03-01 16:39:50 +0530

    Update percentage.c
    percentage updated

--- 3_implementation/src/percentage.c
+++ 3_implementation/src/percentage.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+#include <math.h>
 #include "percentage.h"
 
 void do_percentage() 
 {
-   float percentage;
+   ufloat percentage;
    int deno, numo = 0;
    printf("\nPlease enter numerator  : ");
    scanf("%d", &numo); 
    printf("\nPlease enter denominator  : ");
    scanf("%d", &deno);
 
-   percentage = (float)numo / deno * 100.0;
+   percentage = (fabs(numo * 100) / deno);
 
    printf("Percentage = %.2f%%", percentage);
 
