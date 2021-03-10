366519959b8a264973ff5c81f5669b86066c0e97
99003592 <78539763+99003592@users.noreply.github.com>
2021-03-10 09:04:26 +0530

    issue resolving

--- 3_implementation/src/addition.c
+++ 3_implementation/src/addition.c
@@ -3,11 +3,12 @@
 
 void do_addition()
 {
-    int n;
-    float total=0, k=0, number;
+    int n, k=0;
+    float total=0,  number;
     printf("\nEnter the number of elements you want to add:");
     scanf("%d",&n);
     printf("Please enter %d numbers one by one: \n",n);
+    
     if(n>0){
         while(k<n)
         { 
