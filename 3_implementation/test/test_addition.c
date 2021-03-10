b84435236db628ac6e3770ee1bdf69cf7b8a5254
99003592 <78539763+99003592@users.noreply.github.com>
2021-03-10 09:19:06 +0530

    addition test cases added
    boundary condition test requirements added to the code.

--- 3_implementation/test/test_addition.c
+++ 3_implementation/test/test_addition.c
@@ -10,6 +10,14 @@ void test_addition_basic(void)
 	TEST_ASSERT_EQUAL( 36, addition(h01_n, (int*)&h01_arr));
 }
 
+void test_addition_advance(void)
+{
+    // H_01
+    int h01_arr[] = {1200000, 24111111};
+    int h01_n = sizeof(h01_arr)/sizeof(int);
+	TEST_ASSERT_EQUAL( 25311111, addition(h01_n, (int*)&h01_arr));
+}
+
 int test_addition(void)
 {
 	/* Initiate the Unity Test Framework */
@@ -17,6 +25,7 @@ int test_addition(void)
 
 	/* Run Test functions */
 	RUN_TEST(test_addition_basic);
+	RUN_TEST(test_addition_advance);
 
 	/* Close the Unity Test Framework */
 	return UNITY_END();
